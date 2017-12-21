6d3edba37a8e46bf3ce8212c47e231e8a5f10b21
psbhanu <psbhanu@outlook.com>
2017-12-22 02:21:29 +0530

    Minor issues fixed

--- RRScheduler.cpp
+++ RRScheduler.cpp
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @Package RRScheduler
+ * @Version 1.0.0
+ * @Author Amira M.M
+ * @Licence GNU 2.0
  */
 
 #include <string>
@@ -19,20 +20,20 @@ int RRScheduler::accept(int ch) {
     //%f i want float value 
     //%c i want char value 
     int i, n;
-    cout"enter the number of the process: ";
-    cin>>n;
+    cout << "enter the number of the process: ";
+    cin >> n;
     if (n == 0) {
-        cout<<"invalid no.of process";
+        cout << "invalid no.of process";
         exit(1);
     }
     for (i = 1; i <= n; i++) {
-        cout<<"enter the arrival time for process p%d: "<< i;
-        cin>>p[i].at;
+        cout << "enter the arrival time for process p: " << i;
+        cin >> p[i].at;
         p[i].id = i;
     }
     for (i = 1; i <= n; i++) {
-        cout<<"enter the burst time for process p%d: "<< i;
-        cin<<p[i].bt;
+        cout << "enter the burst time for process p: " << i;
+        cin >> p[i].bt;
     }
     for (i = 1; i <= n; i++) {
         p1[i] = p[i];
@@ -45,7 +46,7 @@ void RRScheduler::ganttrr(int n) {
     nextval = p1[1].at;
     i = 1;
     cout << "enter time slice: ";
-    cin>>ts;
+    cin >> ts;
     for (i = 1; i <= n && p1[i].at <= nextval; i++)
 
         q1.push(p1[i].id);
@@ -93,15 +94,15 @@ void RRScheduler::display(int n)
  {
     string tab = "          ";
     cout << "\n\n-------------------TABLE----------------------------------\n";
-    cout<<"\nProcess" << tab << "AT" << tab << "BT" << tab << "FT" << tab << "TAT" << tab << "WT";
+    cout << "\nProcess" << tab << "AT" << tab << "BT" << tab << "FT" << tab << "TAT" << tab << "WT";
     
     int i;
     for (i = 1; i <= n; i++)
-        cout<<"\nP"<< p[i].id << tab << p[i].at << tab << p[i].bt << tab << p[i].ft << tab << p[i].tat << tab << p[i].wt;
+        cout << "\nP"<< p[i].id << tab << p[i].at << tab << p[i].bt << tab << p[i].ft << tab << p[i].tat << tab << p[i].wt;
     
     cout << "\n-----------------------------------------------------------";
     
-    cout<<"\nAverage Turn Around Time: " << p[0].tat;
-    cout<<"\nAverage Waiting Time: " << p[0].wt;
+    cout << "\nAverage Turn Around Time: " << p[0].tat;
+    cout << "\nAverage Waiting Time: " << p[0].wt;
 }
 
