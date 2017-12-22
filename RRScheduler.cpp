c55e8e963abf0ed052ea3805920315109c1e4673
psbhanu <psbhanu@outlook.com>
2017-12-22 18:06:22 +0530

    Final changes integrated v 1.0

--- RRScheduler.cpp
+++ RRScheduler.cpp
@@ -1,126 +1,147 @@
 /*
- * @Package RRScheduler
+ * @Package RRScheduler (CPP)
  * @Version 1.0.0
  * @Author Amira M.M
  * @Licence GNU 2.0
  */
 
+#include<iostream>
+#include<cstdlib>
+#include<queue>
+#include<cstdio> 
+
 #include <string>
 #include <iomanip>
-#include </home/win/NetBeansProjects/RRSchedular/RRScheduler.h>
+
+#include "RRScheduler.h"
+
+using namespace std;
+typedef struct process {
+    int ProcessID, ArrivalTime, BurstTime, StratTime, FinishTime, Priority;
+    float WaitTime, TurnArroundTime;
+} process;
+
+process Process[10], p1[10], temp;
+queue<int> queue1;
 
 RRScheduler::RRScheduler() {
+    Process[0].TurnArroundTime = 0;
+    Process[0].WaitTime = 0;
 }
 
 int RRScheduler::execute() {
     int i, n, ts, ch, j, x;
-    this->p[0].tat = 0;
-    this->p[0].wt = 0;
-    n = this->accept(ch);
-    this->ganttrr(n);
-    this->turnwait(n);
-    this->display(n);
+
+    n = accept(ch);
+    ganttrr(n);
+    turnwait(n);
+    display(n);
     return 0;
 }
 
-int RRScheduler::accept(int ch) {
-//%d you wants intger value ,
-    //%f i want float value 
-    //%c i want char value 
+int RRScheduler::accept(int ch) //functions for accepting inputs from the user
+{
     int i, n;
-    cout << "enter the number of the process: ";
-    cin >> n;
+    cout << "\nEnter the no. of process: ";
+    scanf("%d", &n);
     if (n == 0) {
-        cout << "invalid no.of process";
+        cout << "\nInvalid no. of process";
         exit(1);
     }
     for (i = 1; i <= n; i++) {
-        cout << "enter the arrival time for process p: " << i;
-        cin >> this->p[i].at;
-        this->p[i].id = i;
+        printf("\nEnter the arrival time for process P%d: ", i);
+        scanf("%d", &Process[i].ArrivalTime);
+        Process[i].ProcessID = i;
     }
     for (i = 1; i <= n; i++) {
-        cout << "enter the burst time for process p: " << i;
-        cin >> this->p[i].bt;
+        printf("\nEnter the burst time for process P%d: ", i);
+        scanf("%d", &Process[i].BurstTime);
     }
-    for (i = 1; i <= n; i++) {
-        this->p1[i] = this->p[i];
+    if (ch == 3) {
+        for (i = 1; i <= n; i++) {
+            printf("\nEnter the priority for P%d: ", i);
+            scanf("%d", &Process[i].Priority);
+        }
         return n;
     }
+
+    printf("\nInputs given by the user are:\n");
+
+    printf("==============================================================");
+    printf("\nProcess\tAT\tBT");
+    for (i = 1; i <= n; i++)
+        printf("\nP%d\t%d\t%d", Process[i].ProcessID, Process[i].ArrivalTime, Process[i].BurstTime);
+
+    printf("\n============================================================");
+    for (i = 1; i <= n; i++) // copying of one array into another array(dummy area)
+        p1[i] = Process[i];
+    return n;
 }
 
+/* FUNCTION FOR DISPLAYING GANTT CHART FOR ROUND ROBIN SCHEDULING */
 void RRScheduler::ganttrr(int n) {
     int i, ts, m, nextval, nextarr;
-    nextval = this->p1[1].at;
-    i = 1;
-    cout << "enter time slice: ";
-    cin >> ts;
-    for (i = 1; i <= n && this->p1[i].at <= nextval; i++)
-        this->q1.push(this->p1[i].id);
+    nextval = Process[1].ArrivalTime;
+    cout << "\nEnter time slice: ";
+    cin>>ts;
 
-    while (!this->q1.empty()) {
-        m = this->q1.front();
-        this->q1.pop();
+    nextval = p1[1].ArrivalTime;
+    printf("\n\nGantt chart is as follows\n\n");
+    cout << p1[1].ArrivalTime;
 
-        if (this->p1[m].bt >= ts)
+    for (i = 1; i <= n, p1[i].ArrivalTime <= nextval; i++)
+        queue1.push(p1[i].ProcessID);
+    nextarr = p1[i].ArrivalTime;
+    while (!queue1.empty()) {
+        m = queue1.front();
+        queue1.pop();
+        if (p1[m].BurstTime >= ts)
             nextval = nextval + ts;
         else
-            nextval = nextval + this->p1[m].bt;
-        if (this->p1[m].bt >= ts)
-            this->p1[m].bt = this->p1[m].bt - ts;
-        else
-            this->p1[m].bt = 0;
-        while (i <= n && this->p1[i].at <= nextval) {
-            this->q1.push(this->p1[i].id);
-            i++;
+            nextval = nextval + p1[m].BurstTime;
+        cout << "->P" << p1[m].ProcessID << "->" << nextval;
+        p1[m].BurstTime = p1[m].BurstTime - ts;
+        if (nextval < nextarr) {
+            if (p1[m].BurstTime > 0)
+                queue1.push(m);
+            if (p1[m].BurstTime <= 0)
+                Process[m].FinishTime = nextval;
+        } else {
+            while (i <= n && p1[i].ArrivalTime <= nextval) {
+                queue1.push(p1[i].ProcessID);
+                i++;
+            }
+            if (i <= n)
+                nextarr = p1[i].ArrivalTime;
+            if (p1[m].BurstTime > 0)
+                queue1.push(m);
+            if (p1[m].BurstTime <= 0)
+                Process[m].FinishTime = nextval;
         }
-
-        if (this->p1[m].bt > 0)
-            this->q1.push(m);
-        if (this->p1[m].bt <= 0)
-            this->p[m].ft = nextval;
     }
-
 }
 
+/* FUNCTION FOR CALCULATING TURN AROUND TIME OR WAIT TIME */
 void RRScheduler::turnwait(int n) {
-
     int i;
     for (i = 1; i <= n; i++) {
-        this->p[i].tat = this->p[i].ft - this->p[i].at;
-        this->p[i].wt = this->p[i].tat - this->p[i].bt;
-        this->p[0].tat = this->p[0].tat + this->p[i].tat;
-        this->p[0].wt = this->p[0].wt + this->p[i].wt;
+        Process[i].TurnArroundTime = Process[i].FinishTime - Process[i].ArrivalTime;
+        Process[i].WaitTime = Process[i].TurnArroundTime - Process[i].BurstTime;
+        Process[0].TurnArroundTime = Process[0].TurnArroundTime + Process[i].TurnArroundTime;
+        Process[0].WaitTime = Process[0].WaitTime + Process[i].WaitTime;
     }
+    Process[0].TurnArroundTime = Process[0].TurnArroundTime / n;
+    Process[0].WaitTime = Process[0].WaitTime / n;
+}
 
-    this->p[0].tat = this->p[0].tat / n;
-    this->p[0].wt = this->p[0].wt / n;
-}
-/*
-void RRScheduler::display(int n)
- {
-    string tab = "";
-    cout << "\n\n-------------------TABLE----------------------------------\n";
-    cout << std::setw(10) << "\nProcess" << std::setw(5) << "AT" << std::setw(10) << "BT" << std::setw(10) << "FT" << std::setw(10) << "TAT" << std::setw(10) << "WT";
-    
+/* FUNCTION FOR DISPLAYING THE TABLE */
+void RRScheduler::display(int n) {
     int i;
+    cout << "\n\n-------------------TABLE----------------------------------\n";
+    printf("\nProcess\tAT\tBT\tFT\tTAT\t\tWT");
     for (i = 1; i <= n; i++)
-        cout << std::setw(10) << "\nP"<< (int) p[i].id << std::setw(10) << (int) p[i].at << std::setw(10) << (int) p[i].bt << std::setw(10) << (int) p[i].ft << std::setw(10) << (float) p[i].tat << "t" << std::setw(10) << (float) p[i].wt << "t";
-    
-    cout << "\n-----------------------------------------------------------";
-    
-    cout << "\nAverage Turn Around Time: " << p[0].tat;
-    cout << "\nAverage Waiting Time: " << p[0].wt;
-}
-*/
-void RRScheduler::display(int n)
-{
- int i; 
- cout<<"\n\n-------------------TABLE----------------------------------\n";
- printf("\nProcess\tAT\tBT\tFT\tTAT\t\tWT");
- for(i=1;i<=n;i++)
-  printf("\nP%d\t%d\t%d\t%d\t%f\t%f", this->p[i].id, this->p[i].at, this->p[i].bt, this->p[i].ft, this->p[i].tat, this->p[i].wt);
- cout<<"\n\n-----------------------------------------------------------";
- printf("\nAverage Turn Around Time: %f", this->p[0].tat);
- printf("\nAverage Waiting Time: %f\n", this->p[0].wt);
+        printf("\nP%d\t%d\t%d\t%d\t%f\t%f", Process[i].ProcessID, Process[i].ArrivalTime, Process[i].BurstTime, Process[i].FinishTime, Process[i].TurnArroundTime, Process[i].WaitTime);
+    cout << "\n\n-----------------------------------------------------------";
+    printf("\nAverage Turn Around Time: %f", Process[0].TurnArroundTime);
+    printf("\nAverage Waiting Time: %f\n", Process[0].WaitTime);
 }
